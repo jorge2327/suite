f4dc68e13aa9c7a1eb5c7bf5029037b922f72e75
jorge <ing-jorgeflores@hotmail.com>
2016-06-09 19:20:42 -0500

    cree input numero

--- formulario.php
+++ formulario.php
@@ -9,6 +9,7 @@
 	<label for="nombre">Nombre	</label><input type="text" name="nombre"><br><br></label>
 	<label for="password">Password 	</label><input type="password" name="pw"><br><br></label>
 	<label for="categoria">Categoria	</label><input type="text" name="cate"><br><br></label>
+	<label for="numero"> Numero	</label><input type="text" name="num"><br><br></label>
 <input type="submit" value="Insertar datos">
 
 
